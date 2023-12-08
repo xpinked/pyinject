9724f36a7286d8cc14bf3c8cca3790b0d1fb9e2b
Udi Shalev <azsd91@gmail.com>
2023-12-08 17:06:30 +0200

    updated tests

--- tests/test_dependency_overrider.py
+++ tests/test_dependency_overrider.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from pyinject import AutoWired, Depends
+from pyinject import AutoWired, Depends, execute as _
 from pyinject.overrider import DependencyOverrider
 
 
@@ -16,11 +16,11 @@ def foo_override():
     return 2
 
 
-@AutoWired
-def func(foo: Annotated[int, Depends(foo)], bar: Annotated[int, Depends(bar)]):
+@AutoWired()
+def func(foo: Annotated[int, Depends(foo)], bar: Annotated[int, Depends(bar)]) -> int:
     return foo + bar
 
 
 def test_autowired__functions() -> None:
     with DependencyOverrider({foo: foo_override}):
-        assert func() == 4  # type: ignore
+        assert _(func) == 4
