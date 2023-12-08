f88590b8f5b7fac0842ff0f46de7313edd4e4665
Udi Shalev <azsd91@gmail.com>
2023-12-08 23:07:36 +0200

    Functionality to override managers,

--- pyinject/overrider.py
+++ pyinject/overrider.py
@@ -1,17 +1,21 @@
 import typing
 
 from .manager import DependenciesManager, OverridesMapping, default_manager
+from .decorators import AutoWired, R, P
 
 
 class DependencyOverrider:
-    def __init__(self, overrides: OverridesMapping, _dependencies_manager: DependenciesManager = default_manager) -> None:
+    def __init__(self, overrides: OverridesMapping, *, manager: DependenciesManager = default_manager) -> None:
         self.overrides = overrides
-        self._dependencies_manager = _dependencies_manager
+        self.manager = manager
         self._old_overrides: OverridesMapping = {}
 
     def __enter__(self):
-        self._old_overrides = self._dependencies_manager.override_dependencies(self.overrides)
+        self._old_overrides = self.manager.override_dependencies(self.overrides)
         return self
 
     def __exit__(self, *args: typing.Any) -> None:
-        self._dependencies_manager.restore_dependencies(self.overrides, self._old_overrides)
+        self.manager.restore_dependencies(self.overrides, self._old_overrides)
+
+    def execute(self, func: typing.Callable[P, R], *args, **kwargs) -> R:
+        return AutoWired(manager=self.manager)(func)(*args, **kwargs)
