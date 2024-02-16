17d20f40a26fc0aff7bb531690d5b52e7372a36a
Udi Shalev <azsd91@gmail.com>
2024-02-16 20:42:17 +0200

    refactoring of AutoWired decorator + small improvement for caching signatures

--- pyinject/decorators.py
+++ pyinject/decorators.py
@@ -9,6 +9,78 @@ R = TypeVar("R", bound=Any)
 P = ParamSpec("P")
 
 
+class _AutoWired:
+    """A class that resolves dependencies from a callable and injects them to arguments Annotations"""
+
+    def __init__(self, manager: DependenciesManager) -> None:
+        self.manager = manager
+
+    def __call__(self, func: Callable[P, R]) -> Callable[P, R]:
+        """Decorator that resolves dependencies from a callable and injects them to arguments Annotations.
+
+        Args:
+        ----
+            func (Callable[P, R]): A callable to be decorated
+
+        Returns:
+        -------
+            Callable[P, R]: A decorated callable
+
+        """
+        sig = inspect.signature(func)
+
+        @wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            bound_args = sig.bind_partial(*args, **kwargs)
+            bound_args.apply_defaults()
+
+            kwargs_after_injection = self._inject_dependencies_to_kwargs(bound_args, sig, kwargs)
+
+            return func(*args, **kwargs_after_injection)
+
+        return wrapper
+
+    def _inject_dependencies_to_kwargs(
+        self,
+        bound_args: inspect.BoundArguments,
+        sig: inspect.Signature,
+        original_kwargs: dict[str, Any],
+    ) -> dict[str, Any]:
+        """Injects dependencies to the arguments Annotations.
+
+        Args:
+        ----
+            bound_args (inspect.BoundArguments): A bound arguments object
+            sig (inspect.Signature): A signature object
+            kwargs (dict[str, Any]): A dictionary containing the keyword arguments
+
+        Returns:
+        -------
+            dict[str, Any]: A dictionary containing the keyword arguments after injection
+
+        """
+        new_kwargs = original_kwargs.copy()
+
+        for param_name, param in sig.parameters.items():
+            if param_name in bound_args.arguments:
+                continue
+
+            if get_origin(param.annotation) is Annotated:
+                _type, _dependency = get_args(param.annotation)
+                _Dependency.validate(_dependency)
+
+                if _dependency.callable is None:
+                    _dependency.callable = _type
+
+                new_kwargs[param_name] = self.manager.get_dependency_value(_dependency)
+                continue
+
+            if param.annotation in self.manager.dependency_overrides:
+                new_kwargs[param_name] = self.manager.get_dependency_value(_Dependency(callable=param.annotation))
+
+        return new_kwargs
+
+
 def AutoWired(*, manager: DependenciesManager = default_manager) -> Callable[[Callable[P, R]], Callable[P, R]]:  # noqa: N802
     """
     A decorator that resolves dependencies from a callable and injects them to arguments Annotations
@@ -22,33 +94,4 @@ def AutoWired(*, manager: DependenciesManager = default_manager) -> Callable[[Ca
         Callable[[Callable[P, R]], Callable[P, R]]: A decorator that resolves dependencies
 
     """
-
-    def decorator(func: Callable[P, R]) -> Callable[P, R]:
-        @wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            sig = inspect.signature(func)
-            bound_args = sig.bind_partial(*args, **kwargs)
-            bound_args.apply_defaults()
-
-            for param_name, param in sig.parameters.items():
-                if param_name in bound_args.arguments:
-                    continue
-
-                if get_origin(param.annotation) is Annotated:
-                    _type, _dependency = get_args(param.annotation)
-                    _Dependency.validate(_dependency)
-
-                    if _dependency.callable is None:
-                        _dependency.callable = _type
-
-                    kwargs[param_name] = manager.get_dependency_value(_dependency)
-                    continue
-
-                if param.annotation is not None and param.annotation in manager.dependency_overrides:
-                    kwargs[param_name] = manager.get_dependency_value(_Dependency(callable=param.annotation))
-
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
+    return _AutoWired(manager=manager)
